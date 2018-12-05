8032be655f8c68b4a24a973a1941dc637a72d4f8
dunahan <25346708+dunahan@users.noreply.github.com>
2018-12-05 21:11:54 +0100

    Update ini.cpp

--- Plugins/Ini/ini.cpp
+++ Plugins/Ini/ini.cpp
@@ -42,9 +42,75 @@ NWNX_PLUGIN_ENTRY Plugin* PluginLoad(Plugin::CreateParams params)
     return g_plugin;
 }
 
-//copied from old c++ source as first test?!
+namespace Ini {
+
+Player::Player(const Plugin::CreateParams& params)
+    : Plugin(params)
+{
+// okay, like most plugins it seems to be good, if i register the (one) function(s) here first? but how exactly?
+#define REGISTER(func) \
+    GetServices()->m_events->RegisterEvent(#func, std::bind(&Ini::func, this, std::placeholders::_1))
+
+    REGISTER(ReadInteger);
+
+#undef REGISTER
+
+//how do i get here around?
+/*    GetServices()->m_hooks->RequestSharedHook
+        <Functions::CNWSMessage__HandlePlayerToServerInputCancelGuiTimingEvent,
+            int32_t, CNWSMessage*, CNWSPlayer*>(&HandlePlayerToServerInputCancelGuiTimingEventHook);
+// do i need that? */
+}
+
+/*copied from old c++ source as help?! old style, how to get that work with new nwnx:ee
 int CIniReader::ReadInteger(char* sSection, char* sKey, int iDefaultValue)
 {
-    int iResult = GetPrivateProfileInt(sSection,  sKey, iDefaultValue, m_sFileName); 
+    int iResult = GetPrivateProfileInt(sSection,  sKey, iDefaultValue, m_sFileName); //okay, this is a definied  c++ func
     return iResult;
+}*/
+    
+/* // a examle from another registered function of player plugin? how helpful could it get, for getting a integer?
+ArgumentStack Player::ForcePlaceableExamineWindow(ArgumentStack&& args)
+{
+    ArgumentStack stack;
+    if (auto *pPlayer = player(args))
+    {
+        const auto placeableId = Services::Events::ExtractArgument<Types::ObjectID>(args);
+
+        auto *pMessage = static_cast<CNWSMessage*>(Globals::AppManager()->m_pServerExoApp->GetNWSMessage());
+        if (pMessage)
+        {
+            pMessage->SendServerToPlayerExamineGui_PlaceableData(pPlayer, placeableId);
+        }
+        else
+        {
+            LOG_ERROR("Unable to get CNWSMessage");
+        }
+    }
+
+    return stack;
 }
+//or is this the right way? from plugin damage...
+int32_t Damage::OnApplyDamage(NWNXLib::API::CNWSEffectListHandler *pThis, NWNXLib::API::CNWSObject *pObject, NWNXLib::API::CGameEffect *pEffect, bool bLoadingGame)
+{
+    Damage& plugin = *g_plugin;
+    auto posScript = plugin.GetServices()->m_perObjectStorage->Get<std::string>(pObject, "DAMAGE_EVENT_SCRIPT");
+    std::string script = posScript ? *posScript : plugin.m_DamageScript;
+
+    if (!script.empty())
+    {
+        // We only run the OnDamage event for creatures.
+        if (Utils::AsNWSCreature(pObject))
+        {
+            // Prepare the data for the nwscript
+            plugin.m_DamageData.oidDamager = pEffect->m_oidCreator;
+
+            std::memcpy(plugin.m_DamageData.vDamage, pEffect->m_nParamInteger, sizeof(plugin.m_DamageData.vDamage));
+            Utils::ExecuteScript(script, pObject->m_idSelf);
+            std::memcpy(pEffect->m_nParamInteger, plugin.m_DamageData.vDamage, sizeof(plugin.m_DamageData.vDamage));
+        }
+    }
+
+    return plugin.m_OnApplyDamageHook->CallOriginal<int32_t>(pThis, pObject, pEffect, bLoadingGame);
+}
+*/
